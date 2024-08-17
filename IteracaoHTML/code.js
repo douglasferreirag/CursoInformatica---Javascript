e0520934d2855f53c84ef5a8fe332d946e5b12a4
douglasferreirag <douglas.ferreirag@gmail.com>
2024-08-16 22:05:55 -0300

    Html e Javascript

---
+++ IteracaoHTML/code.js
@@ -0,0 +1,27 @@
+
+
+function ExibirNota1(){
+ 
+     document.write(document.getElementById('n1').value);
+
+}
+
+function ExibirNota2(){
+
+
+    document.write(document.getElementById('n2').value);
+
+}
+
+function ExibirNota3(){
+    
+
+    document.write(document.getElementById('n3').value);
+
+}
+
+function ExibirNota4(){
+    
+     document.write(document.getElementById('n4').value);
+
+}
