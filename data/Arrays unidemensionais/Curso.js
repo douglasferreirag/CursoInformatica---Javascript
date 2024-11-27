c89c5a810beb3ba5149ec51d666417d4cdef9e42
unknown <douglas.ferreirag@gmail.com>
2024-11-27 15:03:12 -0300

    mocks com array

---
+++ data/Arrays unidemensionais/Curso.js
@@ -0,0 +1,18 @@
+const python = [
+
+    "p5vdcx",
+    "Python",
+    "Avançado"
+
+];
+
+const windows10 = [
+
+    "p5vdcx1%()",
+    "Windows10",
+    "Basico"
+
+]
+
+
+
---
+++ data/Arrays unidemensionais/Endereco.js
@@ -0,0 +1,17 @@
+const Ender1 = [
+
+    "i89-0",
+    "Rua da Imprensa",
+    "Campo Grande",
+    "Rio de Janeiro"
+
+]
+
+const Ender2 = [
+
+    "iABC-013",
+    "Rua Frederico Moura",
+    "Franca",
+    "Sao Paulo"
+
+]
